0a8b08d3869c99182146d016bd7d327092f55766
rares <colorlord98@gmail.com>
2021-04-02 15:30:54 +0300

    Separated speaker and listener to their own class called participant

--- ConferenceManagementSystem/src/main/kotlin/server/domain/Section.kt
+++ ConferenceManagementSystem/src/main/kotlin/server/domain/Section.kt
@@ -7,8 +7,6 @@ data class Section(
     val conferenceId: Int,
     var sessionChairId: Int,
     var eventDate: Date,
-    var speakers: List<Int>,
-    var listeners: List<Int>,
     var roomId: Int
 ) :
     BaseEntity<Int>(id)
