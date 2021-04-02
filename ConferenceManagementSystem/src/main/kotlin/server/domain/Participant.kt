039463dda42c498f89525a2fb904766d7e38ab7d
rares <colorlord98@gmail.com>
2021-04-02 15:32:39 +0300

    Literally missed the file I was supposed to add in the last commit -_-

---
+++ ConferenceManagementSystem/src/main/kotlin/server/domain/Participant.kt
@@ -0,0 +1,8 @@
+package server.domain
+
+data class Participant(
+    val userId: Int,
+    val sectionId: Int,
+    var isSpeaker: Boolean
+) :
+    BaseEntity<Pair<Int, Int>>(Pair(userId, sectionId))
