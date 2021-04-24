eb87188df51d5ca110a46a31d74a1c200cc92b1c
ddbl <34249170+ddbl@users.noreply.github.com>
2021-04-24 15:45:42 +0300

    add ChatView

---
+++ ConferenceManagementSystem/src/main/kotlin/client/view/chat/ChatView.kt
@@ -0,0 +1,31 @@
+package client.view.chat
+
+import javafx.beans.value.ObservableStringValue
+import javafx.geometry.Pos
+import tornadofx.*
+import utils.APPLICATION_TITLE
+
+class ChatView : View(APPLICATION_TITLE) {
+
+    override val root = vbox(64.0, Pos.CENTER) {
+        minWidth = 528.0
+        minHeight = 456.0
+
+        vbox(16.0, Pos.CENTER) {
+            listview<ObservableStringValue> {
+                maxWidth = 480.0
+                maxHeight = 280.0
+            }
+            textarea {
+                maxWidth = 480.0
+                maxHeight = 80.0
+            }
+
+            vbox(alignment = Pos.CENTER_RIGHT) {
+                maxWidth = 480.0
+
+                button("Send")
+            }
+        }
+    }
+}
